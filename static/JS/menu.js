f603fb9c24c5a840e7ad3019a48a75a9c5208585
Ecsplosion <zaidbzaheer2@gmail.com>
2022-01-30 20:06:15 +0500

    FUK

--- static/JS/menu.js
+++ static/JS/menu.js
@@ -1,11 +1,17 @@
-const my_menu = document.getElementsByClassName('menu')[0]
-if(my_menu){
-const toggle_button = document.getElementById('more-options');
-toggle_button.addEventListener('click', function(){
-    if(my_menu.style.bottom == '-80px'){
-        my_menu.style.bottom = '-30px'
+const ipr_openButton = document.getElementById('ipr-button-pc')
+const ipr_closeButton = document.getElementById('modal-close-button')
+const ipr_modal = document.getElementsByClassName('ipr-modal')[0]
+ipr_openButton.addEventListener('click', ()=>{
+    if(ipr_modal.style.display == 'none'){
+        ipr_modal.style.display = 'block';
     } else {
-        my_menu.style.bottom = '-80px'
+        ipr_modal.style.display = 'none';
     }
-})    
-}
+})
+ipr_closeButton.addEventListener('click', ()=> {
+    if(ipr_modal.style.display == 'block'){
+        ipr_modal.style.display = 'none';
+    } else {
+        ipr_modal.style.display = 'block';
+    }
+})
