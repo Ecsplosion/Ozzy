ae7d647bd3fbc2973e6782fe307cc726de4c9217
Ecsplosion <zaidbzaheer2@gmail.com>
2022-01-30 16:59:28 +0500

    CSS ADVANCED

--- static/JS/menu.js
+++ static/JS/menu.js
@@ -3,7 +3,7 @@ if(my_menu){
 const toggle_button = document.getElementById('more-options');
 toggle_button.addEventListener('click', function(){
     if(my_menu.style.bottom == '-80px'){
-        my_menu.style.bottom = '-27px'
+        my_menu.style.bottom = '-30px'
     } else {
         my_menu.style.bottom = '-80px'
     }
